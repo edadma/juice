0e659047a53071f09833bca62fdf07c87b83862b
Edward A Maxedon, Sr <edadma@gmail.com>
2021-10-06 20:52:44 -0400

    code basic site processing

--- shared/src/main/scala/io/github/edadma/juice/App.scala
+++ shared/src/main/scala/io/github/edadma/juice/App.scala
@@ -1,19 +1,14 @@
 package io.github.edadma.juice
 
 import java.nio.file.{Files, Path}
-
 import scala.collection.immutable.VectorMap
 import scala.jdk.CollectionConverters._
 import scala.language.postfixOps
-
 import io.github.edadma.cross_platform.readFile
+import io.github.edadma.squiggly.platformSpecific.yaml
+import org.ekrich.config.{Config, ConfigFactory, ConfigParseOptions, ConfigSyntax}
 
-import org.ekrich.config.{
-  Config,
-  ConfigFactory,
-  ConfigParseOptions,
-  ConfigSyntax,
-}
+import scala.annotation.tailrec
 
 object App {
 
@@ -38,20 +33,55 @@ object App {
   val run: PartialFunction[Command, Unit] = {
     case BuildCommand(src, dst) =>
       val conf = new ConfigWrapper(config(src))
-      val layoutdir = conf.list.layoutdir
 
-      println(layoutdir)
+      processDir(src, dst, conf)
     case ConfigCommand(src) => printConfig(config(src))
+  }
+
+  case class ContentFile(data: Any, content: String)
+
+  def processDir(src: Path, dst: Path, conf: ConfigWrapper): Unit = {
+    val md =
+      listFiles(src, "md", "markdown") map { p =>
+        val s = readFile(p.toString)
+        val lines = scala.io.Source.fromString(s).getLines()
+        val data =
+          lines.next() match {
+            case "---" =>
+              val buf = new StringBuilder
+
+              @tailrec
+              def line(): Unit =
+                if (lines.hasNext) {
+                  lines.next() match {
+                    case "---" =>
+                    case s =>
+                      buf ++= s
+                      buf += '\n'
+                      line()
+                  }
+                } else
+                  problem(s"unexpected end of file which reading front matter: $p")
+
+              line()
+              buf.toString
+            case _ => ""
+          }
+
+        ContentFile(yaml(data), lines map (_ :+ '\n') mkString)
+      }
+
+    println(md)
   }
 
   def renderValue(v: Any): String =
     v match {
-      case s: String => s"${'"'}$s${'"'}"
-      case n: Int => n.toString
-      case n: Double => n.toString
-      case b: Boolean => b.toString
-      case l: List[_] => l map renderValue mkString("[", ", ", "]")
-      case m: VectorMap[_, _] => m map { case (k, v) => s"$k: ${renderValue(v)}" } mkString("{", ", ", "}")
+      case s: String          => s"${'"'}$s${'"'}"
+      case n: Int             => n.toString
+      case n: Double          => n.toString
+      case b: Boolean         => b.toString
+      case l: List[_]         => l map renderValue mkString ("[", ", ", "]")
+      case m: VectorMap[_, _] => m map { case (k, v) => s"$k: ${renderValue(v)}" } mkString ("{", ", ", "}")
     }
 
   def printConfig(c: Config): Unit =
@@ -62,12 +92,12 @@ object App {
     val suffixes = exts map ('.' +: _)
 
     Files.list(dir).iterator.asScala.toList filter (p =>
-      isFile(p) && (suffixes.isEmpty || suffixes.exists(p.getFileName.toString endsWith _)))
+      isFile(p) && (suffixes.isEmpty || suffixes.exists(p.getFileName.toString endsWith _))) sortBy (_.getFileName.toString)
   }
 
   def extension(filename: String): String =
     filename lastIndexOf '.' match {
-      case -1 => ""
+      case -1  => ""
       case dot => filename substring (dot + 1)
     }
 
@@ -75,8 +105,8 @@ object App {
     val file = path.toString
     val syntax =
       extension(file) match {
-        case "json" => ConfigParseOptions.defaults.setSyntax(ConfigSyntax.JSON)
-        case "conf" | "hocon" => ConfigParseOptions.defaults.setSyntax(ConfigSyntax.CONF)
+        case "json"                 => ConfigParseOptions.defaults.setSyntax(ConfigSyntax.JSON)
+        case "conf" | "hocon"       => ConfigParseOptions.defaults.setSyntax(ConfigSyntax.CONF)
         case "props" | "properties" => ConfigParseOptions.defaults.setSyntax(ConfigSyntax.PROPERTIES)
       }
 
