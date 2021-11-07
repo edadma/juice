a1773626fb0daa1bc0224dff2748458319d33b39
Edward A Maxedon Sr <edadma@gmail.com>
2021-11-07 12:01:29 -0500

    complete shortcode preprocessor

--- shared/src/main/scala/io/github/edadma/juicer/Preprocessor.scala
+++ shared/src/main/scala/io/github/edadma/juicer/Preprocessor.scala
@@ -13,14 +13,17 @@ class Preprocessor(startDelim: String = "{{",
                    shortcodes: TemplateLoader,
                    renderer: TemplateRenderer) {
 
+  case class Shortcode(pos: CharReader, name: String, attrs: Seq[(Ident, Option[String])], buf: StringBuilder)
+
   def process(content: String): String = {
     val buf = new StringBuilder
-    val stack = new mutable.Stack
+    val stack = new mutable.Stack[Shortcode]
 
     @tailrec
     def process(r: CharReader): Unit = {
-      def render(name: String, attrs: Seq[(Ident, Option[String])]): Unit = {
-        val data = attrs map { case (Ident(_, k), v) => k -> v.getOrElse("true") } toMap
+      def render(name: String, attrs: Seq[(Ident, Option[String])], content: Option[String]): Unit = {
+        val data = (attrs map { case (Ident(_, k), v) => k -> v.getOrElse("true") }) ++ (content map (s =>
+          List("content" -> s)) getOrElse Nil) toMap
 
         shortcodes(name) match {
           case Some(template) =>
@@ -28,7 +31,8 @@ class Preprocessor(startDelim: String = "{{",
             val out = new PrintStream(code)
 
             renderer.render(data, template, out)
-            buf ++= code.toString
+
+            (if (stack.isEmpty) buf else stack.top.buf) ++= code.toString
           case None => r.error(s"unknown shortcode: $name")
         }
       }
@@ -37,19 +41,32 @@ class Preprocessor(startDelim: String = "{{",
         r.matchDelimited(startDelim, endDelim) match {
           case Some(Some((shortcode, rest))) =>
             new ShortcodeParser(shortcode, r.line, r.col).parseShortcode match {
-              case ShortcodeStartAST(Ident(pos, name), attrs, closed) =>
+              case ShortcodeStartAST(Ident(_, name), attrs, closed) =>
                 if (closed)
-                  render(name, attrs)
-              case ShortcodeEndAST(Ident(_, name)) =>
+                  render(name, attrs, None)
+                else
+                  stack push Shortcode(r, name, attrs, new StringBuilder)
+              case ShortcodeEndAST(Ident(_, endname)) =>
+                println("end", endname)
+                val Shortcode(pos, name, attrs, buf) = stack.pop()
+
+                if (name != endname)
+                  pos.error(s"shortcode end tag name does not match start name: $name")
+
+                render(name, attrs, Some(buf.toString))
             }
           case Some(None) =>
-            buf += r.ch
+            (if (stack.isEmpty) buf else stack.top.buf) += r.ch
             process(r.next)
           case None => r.error("unclosed shortcode")
         }
     }
 
     process(CharReader.fromString(content))
+
+    if (stack.nonEmpty)
+      stack.top.pos.error(s"unclosed shortcode '${stack.top.name}'")
+
     buf.toString
   }
 
